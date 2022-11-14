7e8b003fccac3f3ec746f8e8232ed79a49a5fab0
Alexander <sany199574@gmail.com>
2022-11-15 00:17:06 +0300

    Added CommonDefinition class
    responsible for general configuration

---
+++ TechMarket.WEB/Definitions/Common/CommonDefinition.cs
@@ -0,0 +1,35 @@
+﻿using TechMarket.WEB.Definitions.Base;
+
+namespace TechMarket.WEB.Definitions.Common
+{
+    public class CommonDefinition : AppDefinitions
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public override void ConfigureApplication(WebApplication app, IWebHostEnvironment environment)
+        {
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
