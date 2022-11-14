976299b3a6a0b533b9583feee9ca05ecb29fe98c
Alexander <sany199574@gmail.com>
2022-11-15 00:10:34 +0300

    Abstract class added AppDefinitions -Serves for sequential redefinition of methods -for new configurations
    Added static classAppDefinitionExtensions
    -Used to connect all collections of definitions

---
+++ TechMarket.WEB/Definitions/Base/AppDefinitionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using TechMarket.WEB.Definitions.Base.Interfaces;
+
+namespace TechMarket.WEB.Definitions.Base
+{
+    public static class AppDefinitionExtensions
+    {
+        public static void AddDefinitions(this IServiceCollection source, WebApplicationBuilder builder, params Type[] entryPointAssembly)
+        {
+            var definitions = new List<IAppDefinitions>();
+
+            foreach (var entryPoin in entryPointAssembly)
+            {
+                var types = entryPoin
+                    .Assembly
+                    .ExportedTypes
+                    .Where(x => !x.IsAbstract && typeof(IAppDefinitions).IsAssignableFrom(x));
+
+                var instances = types.Select(Activator.CreateInstance).Cast<IAppDefinitions>();
+
+                definitions.AddRange(instances);
+            }
+
+            definitions.ForEach(app => app.ConfigureServices(source, builder.Configuration));
+            source.AddSingleton(definitions as IReadOnlyCollection<IAppDefinitions>);
+
+        }
+
+        public static void UseDefinitions(this WebApplication source)
+        {
+            var definitions = source.Services.GetRequiredService<IReadOnlyCollection<IAppDefinitions>>();
+            var environment = source.Services.GetRequiredService<IWebHostEnvironment>();
+            foreach (var endpoint in definitions)
+            {
+                endpoint.ConfigureApplication(source, environment);
+            }
+        }
+    }
+}
---
+++ TechMarket.WEB/Definitions/Base/AppDefinitions.cs
@@ -0,0 +1,11 @@
+﻿using TechMarket.WEB.Definitions.Base.Interfaces;
+
+namespace TechMarket.WEB.Definitions.Base
+{
+    public abstract class AppDefinitions : IAppDefinitions
+    {
+        public virtual void ConfigureApplication(WebApplication app, IWebHostEnvironment environment) { }
+
+        public virtual void ConfigureServices(IServiceCollection services, IConfiguration configuration) { }
+    }
+}
