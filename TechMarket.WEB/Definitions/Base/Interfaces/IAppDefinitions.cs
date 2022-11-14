a7c372d0b9f83006c0067988a675889625bfbdd0
Alexander <sany199574@gmail.com>
2022-11-14 23:54:36 +0300

    Added IAppDefinitions interface
    It is needed for methods that will configure our application and our services.

---
+++ TechMarket.WEB/Definitions/Base/Interfaces/IAppDefinitions.cs
@@ -0,0 +1,9 @@
+﻿namespace TechMarket.WEB.Definitions.Base.Interfaces
+{
+    public interface IAppDefinitions
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration);
+
+        public void ConfigureApplication(WebApplication app, IWebHostEnvironment environment);
+    }
+}
